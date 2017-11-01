d6286cdd7946c9ca2d24f6dbd450287d74eb278c
Andy Bryant <anbryant@academyart.edu>
2017-11-01 10:29:51 -0700

    stuff

--- test/test-server-recipes.js
+++ test/test-server-recipes.js
@@ -3,33 +3,17 @@ const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server');
 
-// this lets us use *should* style syntax in our tests
-// so we can do things like `(1 + 1).should.equal(2);`
-// http://chaijs.com/api/bdd/
 const should = chai.should();
 
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
 
 describe('Shopping List', function() {
 
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
-  // running before our server has started.
   before(function() {
     return runServer();
   });
 
-  // although we only have one test module at the moment, we'll
-  // close our server at the end of these tests. Otherwise,
-  // if we add another test module that also has a `before` block
-  // that starts our server, it will cause an error because the
-  // server would still be running from the previous tests.
   after(function() {
     return closeServer();
   });
@@ -38,6 +22,7 @@ describe('Shopping List', function() {
   //   1. make request to `/shopping-list`
   //   2. inspect response object and prove has right code and have
   //   right keys in response object.
+
   it('should list items on GET', function() {
     // for Mocha tests, when we're dealing with asynchronous operations,
     // we must either return a Promise object or else call a `done` callback
@@ -135,6 +120,43 @@ describe('Shopping List', function() {
       })
       .then(function(res) {
         res.should.have.status(204);
+      });
+  });
+});
+
+
+describe('Shopping List', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('should list recipes on GET', function() {
+    // for Mocha tests, when we're dealing with asynchronous operations,
+    // we must either return a Promise object or else call a `done` callback
+    // at the end of the test. The `chai.request(server).get...` call is asynchronous
+    // and returns a Promise, so we just return it.
+    return chai.request(app)
+      .get('/recipes')
+      .then(function(res) {
+        console.log('running');
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+
+        // because we create three items on app load
+        res.body.length.should.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `checked`.
+        const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
       });
   });
 });
